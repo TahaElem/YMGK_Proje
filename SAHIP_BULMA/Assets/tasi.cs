31bd418526faeed1adb8365acfb03804f6f5b52c
TahaElem <88631343+TahaElem@users.noreply.github.com>
2023-04-19 07:48:00 +0300

    ilk_deneme
    Proje ara yüz ve grafiklerin eklenmesi

---
+++ SAHIP_BULMA/Assets/tasi.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tasi : MonoBehaviour
+{
+    Camera kamera;
+    Vector2 baslangic_pos;
+    GameObject[] kutu_dizisi;
+
+
+    private void OnMouseDrag() 
+    {
+        Vector3 pozisyon = kamera.ScreenToWorldPoint(Input.mousePosition);
+        pozisyon.z = 0;
+        transform.position = pozisyon;
+    
+    }
+    void Start()
+    {
+        kamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        baslangic_pos = transform.position;
+        kutu_dizisi = GameObject.FindGameObjectsWithTag("kutu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            foreach (GameObject kutu in kutu_dizisi)
+            {
+
+                if (kutu.name==gameObject.name)
+                {
+
+                    float mesafe = Vector3.Distance(kutu.transform.position, transform.position);
+                    if (mesafe<=1)
+                    {
+                        transform.position = kutu.transform.position;
+
+                    }
+                    else
+                    {
+                        transform.position = baslangic_pos;
+                    }
+
+                }
+            }
+
+        }
+        
+    }
+}
